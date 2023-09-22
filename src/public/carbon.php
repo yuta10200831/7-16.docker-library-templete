d08682fcea66ddf7cb11f6901528adcbe93a3997
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-09-22 16:22:23 +0900

    carbon 問題①

---
+++ src/public/carbon.php
@@ -0,0 +1,18 @@
+<?php
+require '/var/www/html/vendor/autoload.php';
+
+use Carbon\Carbon;
+
+// 現在の日時を取得
+$now = Carbon::now();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Carbon Test</title>
+</head>
+<body>
+    <h1><?php echo $now->format('Y年m月d日 H:i:s'); ?></h1>
+</body>
+</html>
